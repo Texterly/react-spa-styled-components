aaf7f1d0e77639c48258e0ea8b0036b6d8ece2da
Texterly <88582978+Texterly@users.noreply.github.com>
2023-02-08 15:19:19 +0300

    finish filter

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import {useState, UseEffect} from 'react';
+import {IoArrowBack} from 'react-icons/io5'
 
 const Details = () => {
     let { name } = useParams();
+    const navigate = useNavigate();
+    const [country, setCountry] = useState(null);
+
   return (
-    <div>Details {name}</div>
+    <div>
+      <button onClick={() => navigate(-1)}>
+      <IoArrowBack /> Back
+    </button>
+      Details {name}
+    </div>
   )
 }
 
